f8c4472765cf303bb7a65a22481303c83d954a36
jiyun <404@dxschool.com>
2025-05-29 17:14:15 +0900

    remove API key completely

---
+++ lib/config/api_keys.dart
@@ -0,0 +1,4 @@
+class ApiKeys {
+  // 🔑 여기에 API 키 입력
+  static const String openAiApiKey = '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';
+}
